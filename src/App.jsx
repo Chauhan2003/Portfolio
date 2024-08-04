84637ecd1d9cfbcb66432ea0ad7cd9c401783f76
Gagan Chauhan <gagan2020chauhan@gmail.com>
2024-08-04 17:54:31 +0530

    setup completed successfully

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen flex justify-center items-center text-4xl font-bold">
+      Hello Portfolio!
+    </div>
+  );
+};
+
+export default App;
